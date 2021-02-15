1a4075a6087bd4f99f8559f406597299483059f7
chqiu6 <chucky-qiu@lambdastudents.com>
2021-02-14 19:45:07 -0500

    just started, pushing for now, finishing later

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,11 @@
 // implement your posts router here
+const express = require("express");
+const router = express.Router();
+
+router.get("/", () => {
+    res.json({
+        message : "Router test"
+    });
+})
+
+model.exports = router; 
--- api/server.js
+++ api/server.js
@@ -1,2 +1,14 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require("express");
+const postRouter = require("./posts/posts-router"); 
+const postModel = require("./posts/posts-model");
+
+const server = express();
+
+server.use(express.json());
+server.use(postRouter);
+server.use(postModel);
+
+model.exports = server; 
